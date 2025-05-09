c3763321988e46e8ddf053894e25b54c607a3631
KanishaCarter <kanishacarter90@outlook.com>
2025-05-08 20:45:29 -0500

    created additional files and edited app.jsx

---
+++ velour_essence_app/src/components/userAuth/SignUpForm.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import 
+import './form.css'
+
+export function SignUp() {
+    // User's information here
+    const [ firstName , setFirstName ] = useState("")
+    const [ lastName, setLastName ] = useState("")
+    const [ address , setAddress ] = useState("")
+    const [ email , setEmail ]  = useState("")
+    const [ phoneNumber , setPhoneNumber ] = useState(("")
+    const [ userName , setUserName ] = useState("")
+    const [ password , setPassword ] = useState("")
+
+    async function AssignUserInfo() {
+        
+    }
+
+}
+
+
+
+
+export default Login
---
+++ velour_essence_app/src/components/userAuth/signupform.js
@@ -0,0 +1,26 @@
+// List of users as objects
+
+export const usersList = [
+    { firstName: "Kanisha",
+        lastName: "Carter",
+        address: "209 Nailwood Rd, Euless, TX 76039",
+        phoneNumber: 8178614698,
+        email: "email@email.com",
+        password: "8 characters"
+    },
+     { firstName: "Jane",
+        lastName: "Doe",
+        address: "364 Sugar Rd, Hurst, TX 76047",
+        phoneNumber: 8178642158,
+        email: "email@email.com",
+        password: "8 characters"
+    },
+     { firstName: "Ashley",
+        lastName: "Sullivan",
+        address: "418 Baylon Blvd, Fort Worth, TX 76013",
+        phoneNumber: 5555555555,
+        email: "email@email.com",
+        password: "8 characters"
+    },       
+];
+
